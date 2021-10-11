28b5f038187ef07aa5bb3d969fc9dd10f4080694
Ari Utomo [Home] <robertus.ari.utomo@gmail.com>
2021-10-12 02:53:20 +0700

    Change structure

---
+++ Pong/AdvancedPong/Assets/Scripts/Ball.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private Rigidbody2D physic;
+
+    public float force;
+
+    private Vector2 trajectoryOrigin;
+
+    void Start()
+    {
+        physic = GetComponent<Rigidbody2D>();
+
+        trajectoryOrigin = transform.position;
+        RestartGame();
+    }
+
+    void ResetBall()
+    {
+        transform.position = Vector2.zero;
+        physic.velocity = Vector2.zero;
+    }
+
+    void PushBall()
+    {
+        float angle = Random.Range(-45f, 45f);
+        Vector2 dir = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+
+        physic.AddForce(dir * force);
+    }
+
+    void RestartGame()
+    {
+        ResetBall();
+
+        Invoke("PushBall", 2);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        trajectoryOrigin = transform.position;
+    }
+
+    public Vector2 TrajectoryOrigin
+    {
+        get { return trajectoryOrigin; }
+    }
+}
---
+++ Pong/AdvancedPong/Assets/Scripts/PaddleControl.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleControl : MonoBehaviour
+{
+    [SerializeField] private int score;
+
+    [Header("Properties")]
+    [SerializeField] private KeyCode moveUp;
+    [SerializeField] private KeyCode moveDown;
+    [SerializeField] private BoundaryMove boundary;
+    [SerializeField] private float movementSpeed;
+
+    private Vector2 lastPoint;
+    private Rigidbody2D physic;
+    private ContactPoint2D lastContactPoint;
+
+    private void Start()
+    {
+        physic = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float calculatedSpeed = 0f;
+
+        if (Input.GetKey(moveUp))
+        {
+            calculatedSpeed += movementSpeed;
+        }
+
+        if (Input.GetKey(moveDown))
+        {
+            calculatedSpeed -= movementSpeed;
+        }
+
+        physic.velocity = new Vector2(0f, calculatedSpeed);
+
+        lastPoint = transform.position;
+        boundary.RestrictMove(ref lastPoint);
+        transform.position = lastPoint;
+    }
+
+    public void AddScore(int score)
+    {
+        this.score += score;
+    }
+
+    public void ResetScore()
+    {
+        score = 0;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ball"))
+        {
+            lastContactPoint = collision.GetContact(0);
+        }
+    }
+
+    public int Score
+    {
+        get { return score; }
+    }
+
+    public ContactPoint2D LastContactPoint
+    {
+        get { return lastContactPoint; }
+    }
+}
